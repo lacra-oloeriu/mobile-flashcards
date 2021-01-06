46c8b4577ecbcdf0ba058ab88c995f07263e4ace
Lacramioara <lacramioara.oloeriu@gmail.com>
2021-01-06 19:19:10 +0100

    Dispatched create deck

--- src/components/CreateDeck.js
+++ src/components/CreateDeck.js
@@ -4,31 +4,39 @@ import { ScreenContainer } from "./ScreenContainer";
 import { connect } from "react-redux";
 import { addDeck } from "../data/actions/decks";
 import { TextInput } from "react-native-gesture-handler";
-
-function submitCreateDeck({ dispatch, title, navigation }) {
-  
-  if (title) {
-    navigation.navigate("Decks")
-  } 
-}
+import { v4 as uuidv4 } from "uuid";
+import { handleAddDeck } from "../data/actions/decks";
 
 function CreateDeck({ decks, dispatch, navigation }) {
   const [deckTitle, setDeckTitle] = useState("");
+
+  let myRef = React.createRef();
+
+  function submitCreateDeck() {
+    if (deckTitle) {
+      let uuid = uuidv4();
+
+      dispatch(handleAddDeck(deckTitle, uuid));
+
+      myRef.current.clear();
+      setDeckTitle("");
+
+      navigation.navigate("Decks");
+    }
+  }
 
   return (
     <ScreenContainer>
       <Text>New deck title</Text>
       <TextInput
         placeholder="Deck title"
+        ref={myRef}
         onChangeText={(title) => {
           setDeckTitle(title);
         }}
       />
       <Text>{deckTitle}</Text>
-      <Button
-        title="Create"
-        onPress={() => submitCreateDeck({ dispatch, title: deckTitle, navigation })}
-      />
+      <Button title="Create" onPress={() => submitCreateDeck()} />
     </ScreenContainer>
   );
 }
--- src/data/actions/decks.js
+++ src/data/actions/decks.js
@@ -1,3 +1,5 @@
+import { saveDeck } from "../../utils/api";
+
 export const RECEIVE_DECKS = "RECEIVE_DECKS";
 export const ADD_DECK = "ADD_DECK";
 
@@ -13,4 +15,11 @@ export function addDeck(deck) {
     type: ADD_DECK,
     deck,
   };
+}
+
+export function handleAddDeck (title, deckId){
+    return (dispatch, getState)=>{
+        return saveDeck({title, deckId})
+            .then((deck)=>dispatch(addDeck(deck)))
+    }
 }
