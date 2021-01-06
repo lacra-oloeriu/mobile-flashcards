e23fccfcf1861d147a0abc860bc1ea732a0c66d7
Lacramioara <lacramioara.oloeriu@gmail.com>
2021-01-06 13:14:31 +0100

    deck list connected

--- App.js
+++ App.js
@@ -4,13 +4,14 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { DeckList } from "./src/components/DeckList";
+import { DeckListComponent } from "./src/components/DeckList";
 import { CreateDeckComponent } from "./src/components/CreateDeck";
 import { _getDecks, _getQuestions } from "./src/utils/_data";
-import { createStore } from "redux";
+import { createStore , applyMiddleware} from "redux";
 import { Provider } from "react-redux";
 import reducer from "./src/data/reducers";
 import middleware from './src/data/middleware'
+import {handleInitialData} from './src/data/actions/shared'
 
 const Tabs = createBottomTabNavigator();
 const store = createStore(reducer, middleware);
@@ -20,7 +21,7 @@ const store = createStore(reducer, middleware);
 export default class App extends React.Component {
 
   componentDidMount(){
-    store.dispatch()
+    store.dispatch(handleInitialData())
   }
 
   render() {
@@ -28,7 +29,7 @@ export default class App extends React.Component {
       <Provider store={store}>
         <NavigationContainer>
           <Tabs.Navigator>
-            <Tabs.Screen name="Decks" component={DeckList} />
+            <Tabs.Screen name="Decks" component={DeckListComponent} />
             <Tabs.Screen name="AddDeck" component={CreateDeckComponent} />
           </Tabs.Navigator>
         </NavigationContainer>
--- src/components/DeckList.js
+++ src/components/DeckList.js
@@ -3,49 +3,17 @@ import { View, Text, FlatList } from "react-native";
 import { ScreenContainer } from "./ScreenContainer";
 import { _getDecks } from "../utils/_data";
 import { _getQuestions } from "../utils/_data";
+import { connect } from "react-redux";
 
-export class DeckList extends React.Component {
-  noDecks = {};
-  noQuestions = {};
-
-  state = {
-    decks: this.noDecks,
-    questions: this.noQuestions,
-  };
-
-  async componentDidMount() {
-    try {
-      let decks = await _getDecks();
-      let questions = await _getQuestions();
-      console.log("did mount decks", decks);
-      console.log("did mount questions", questions);
-
-      this.setState((state) => {
-        return {
-          ...state,
-          [`decks`]: decks,
-          [`questions`]: questions,
-        };
-      });
-    } catch (e) {
-      console.log("We have errors", e);
-    }
-  }
-
-  render() {
-    console.log("state", this.state);
-
-    let decks = this.state.decks;
-    const items = Object.keys(decks).map(function (key) {
-      console.log("key, value", key, decks[key]);
-      return <Text key={key}>{decks[key].title}</Text>;
-    });
-
-    return (
-      <ScreenContainer>
-        <Text>Deck list component</Text>
-        {items}
-      </ScreenContainer>
-    );
-  }
+function DeckList({ decks, dispatch, navigation }) {
+  console.log("deckList", decks)
+  return (
+    <ScreenContainer>
+      <Text>Deck list component</Text>
+    </ScreenContainer>
+  );
 }
+
+export const DeckListComponent = connect((state) => ({ decks: state.decks }))(
+  DeckList
+);
