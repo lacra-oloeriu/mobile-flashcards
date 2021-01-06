9af8afd3b56cdf3d8c6eefcb1744908ff9d11ef0
Lacramioara <lacramioara.oloeriu@gmail.com>
2021-01-06 18:11:03 +0100

    Added simple useState

--- src/components/CreateDeck.js
+++ src/components/CreateDeck.js
@@ -1,21 +1,32 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Button } from "react-native";
 import { ScreenContainer } from "./ScreenContainer";
 import { connect } from "react-redux";
 import { addDeck } from "../data/actions/decks";
+import { TextInput } from "react-native-gesture-handler";
 
 function CreateDeck({ decks, dispatch, navigation }) {
-  
+  const [deckTitle, setDeckTitle] = useState("");
+
+  const [count, setCount] = useState(0);
+
+  console.log(deckTitle);
   return (
     <ScreenContainer>
-      <Text>Create deck component</Text>
+      <Text>New deck title</Text>
+      <TextInput
+        placeholder="Deck title"
+        onChangeText={(title) => {
+          setDeckTitle(title);
+        }}
+      />
+      <Text>{deckTitle}</Text>
+      <Text>You clicked {count} times</Text>
+      <Button title="Press me" onPress={() => setCount(count + 1)} />
     </ScreenContainer>
   );
 }
 
-
-
-export const CreateDeckComponent = connect((state) => ({ decks:state.decks }))(
+export const CreateDeckComponent = connect((state) => ({ decks: state.decks }))(
   CreateDeck
 );
-
